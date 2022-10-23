c8899b0e6a208728f15f101c8f86a72dc32f1d73
victorparamo <victor.h.paramo.24@gmail.com>
2022-10-23 10:11:16 -0500

    fix: client base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: '/yao-client/',
   plugins: [
     react(),
     checker({ typescript: true }),
